5838bf9477d1826e900a7a0785b39e1de452e385
99Astral99 <strik4462@gmail.com>
2022-10-20 20:56:12 +0300

    Added Domain layer

---
+++ GamesWorkshop.Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using GamesWorkshop.Domain.Enum;
+
+namespace GamesWorkshop.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public int Amount { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ GamesWorkshop.Domain/Enum/Category.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesWorkshop.Domain.Enum
+{
+    public enum Category
+    {
+        [Display(Name = "Space Marines")]
+        SpaceMarines = 0,
+
+        [Display(Name = "Armies Of The Imperium")]
+        ArmiesOfTheImperium = 1,
+
+        [Display(Name = "Armies Of Chaos")]
+        ArmiesOfChaos = 2,
+
+        [Display(Name = "Xenos Armies")]
+        XenosArmies = 3,
+
+        [Display(Name = "Terrain")]
+        Terrain = 4,
+
+        [Display(Name = "Ways To Play")]
+        WaysToPlay = 5,
+    }
+}
