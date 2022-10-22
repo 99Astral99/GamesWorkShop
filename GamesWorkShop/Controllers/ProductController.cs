5ac5be344b25f44fde1a49c35c8e8cfe6323dd4e
99Astral99 <strik4462@gmail.com>
2022-10-22 14:20:40 +0300

    Displaying all products from local db / Test

---
+++ GamesWorkShop/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using GamesWorkshop.Domain.Entities;
+using GamesWorshop.DAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamesWorkshop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var response = await _productRepository.Select();
+
+
+            return View(response);
+        }
+    }
+}
