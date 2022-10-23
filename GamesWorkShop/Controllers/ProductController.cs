dca620f0e73d14061dbce66430ac8a61a66ab66c
99Astral99 <strik4462@gmail.com>
2022-10-23 20:56:11 +0300

    Added action methods in ProductController

--- GamesWorkShop/Controllers/ProductController.cs
+++ GamesWorkShop/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using GamesWorkshop.Service.Interfaces;
+﻿using GamesWorkshop.Domain.View.Product;
+using GamesWorkshop.Service.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GamesWorkshop.Controllers
@@ -15,9 +17,70 @@ namespace GamesWorkshop.Controllers
         public async Task<IActionResult> GetProducts()
         {
             var response = await _productService.GetProducts();
+            if (response.StatusCode == Domain.Enums.StatusCode.OK)
+            {
+                return View(response.Data);
+            }
 
+            return RedirectToAction("Error");
+        }
 
-            return View(response.Data);
+        [HttpGet]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var response = await _productService.GetProduct(id);
+            if (response.StatusCode == Domain.Enums.StatusCode.OK)
+            {
+                return View(response.Data);
+            }
+
+            return RedirectToAction("Error");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var response = await _productService.DeleteProduct(id);
+            if (response.StatusCode == Domain.Enums.StatusCode.OK)
+            {
+                return View(response.Data);
+            }
+
+            return RedirectToAction("Error");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Save(int id)
+        {
+            if (id == 0)
+            {
+                return View();
+            }
+            var response = await _productService.GetProduct(id);
+            if (response.StatusCode == Domain.Enums.StatusCode.OK)
+            {
+                return View(response.Data);
+            }
+
+            return RedirectToAction("Error");
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Save(ProductViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                if(vm.Id == 0)
+                {
+                    await _productService.CreateProduct(vm);
+                }
+                else
+                {
+                    await _productService.Edit(vm.Id, vm);
+                }
+            }
+            return RedirectToAction("GetProducts");
         }
     }
 }
