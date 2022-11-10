3516ef796bf0d0c417efc5af9aadbbd327d0b32d
99Astral99 <strik4462@gmail.com>
2022-11-10 20:16:16 +0300

    Changed the Product entity properties

--- GamesWorkshop.Domain/Entities/Product.cs
+++ GamesWorkshop.Domain/Entities/Product.cs
@@ -7,8 +7,16 @@ namespace GamesWorkshop.Domain.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
+        public string? Features { get; set; }
         public double Price { get; set; }
         public int Amount { get; set; }
-        public Category Category { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string ImageSrc { get; set; }
+        public string? Image1 { get; set; }
+        public string? Image2 { get; set; }
+        public string? Image3 { get; set; }
+        public string? Image4 { get; set; }
+        public string? Image5 { get; set; }
+        public Category? Category { get; set; }
     }
 }
