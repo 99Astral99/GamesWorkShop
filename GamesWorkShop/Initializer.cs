046186c610d3e8421f9657d028bda55ca9e6be2f
99Astral99 <strik4462@gmail.com>
2023-02-03 17:17:48 +0300

    Added class Initializer for DI

---
+++ GamesWorkShop/Initializer.cs
@@ -0,0 +1,31 @@
+﻿using GamesWorkshop.Domain.Entities;
+using GamesWorkshop.Service.Implementations;
+using GamesWorkshop.Service.Interfaces;
+using GamesWorshop.DAL.Interfaces;
+using GamesWorshop.DAL.Repositories;
+
+namespace GamesWorkshop
+{
+	public static class Initializer
+	{
+		public static void InitializeRepositories(this IServiceCollection services)
+		{
+			services.AddScoped<IBaseRepository<Product>, ProductRepository>();
+			services.AddScoped<IBaseRepository<Cart>, CartRepostiroy>();
+			services.AddScoped<IBaseRepository<Order>, OrderRepository>();
+			services.AddScoped<IBaseRepository<User>, UserRepository>();
+			services.AddScoped<IBaseRepository<UserAccount>, UserAccountRepository>();
+			services.AddScoped<IBaseRepository<Contact>, ContactRepository>();
+		}
+
+		public static void InitializeServices(this IServiceCollection services)
+		{
+			services.AddScoped<IProductService, ProductService>();
+			services.AddScoped<ICartService, CartService>();
+			services.AddScoped<IOrderService, OrderService>();
+			services.AddScoped<IAccountService, AccountService>();
+			services.AddScoped<IUserAccountService, UserAccountService>();
+			services.AddScoped<IContactService, ContactService>();
+		}
+	}
+}
--- GamesWorkShop/Program.cs
+++ GamesWorkShop/Program.cs
@@ -1,10 +1,7 @@
+using GamesWorkshop;
 using GamesWorkshop.Domain.Entities;
 using GamesWorkshop.Domain.Mappings;
-using GamesWorkshop.Service.Implementations;
-using GamesWorkshop.Service.Interfaces;
 using GamesWorshop.DAL;
-using GamesWorshop.DAL.Interfaces;
-using GamesWorshop.DAL.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,82 +10,56 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAutoMapper(config =>
 {
-    config.AddProfile(new ProductProfile());
-    config.AddProfile(new UserProfile());
-    config.AddProfile(new UserAccountProfile());
-    config.AddProfile(new OrderProfile());
+	config.AddProfile(new ProductProfile());
+	config.AddProfile(new UserProfile());
+	config.AddProfile(new UserAccountProfile());
+	config.AddProfile(new OrderProfile());
 });
 
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseSqlServer(connectionString, m => m.MigrationsAssembly("GamesWorshop.DAL"));
+	var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+	options.UseSqlServer(connectionString, m => m.MigrationsAssembly("GamesWorshop.DAL"));
 });
 
 
 builder.Services.AddIdentity<User, Role>(opt =>
 {
-    //opt.User.RequireUniqueEmail = true;
-    opt.User.AllowedUserNameCharacters =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+	//opt.User.RequireUniqueEmail = true;
+	opt.User.AllowedUserNameCharacters =
+	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
 
 
-    opt.Password.RequireDigit = false;
-    opt.Password.RequireNonAlphanumeric = false;
-    opt.Password.RequireUppercase = true;
-    opt.Password.RequiredLength = 8;
+	opt.Password.RequireDigit = false;
+	opt.Password.RequireNonAlphanumeric = false;
+	opt.Password.RequireUppercase = true;
+	opt.Password.RequiredLength = 8;
 
-    opt.Lockout = new LockoutOptions
-    {
-        AllowedForNewUsers = true,
-        DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5),
-        MaxFailedAccessAttempts = 5
-    };
+	opt.Lockout = new LockoutOptions
+	{
+		AllowedForNewUsers = true,
+		DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5),
+		MaxFailedAccessAttempts = 5
+	};
 })
-                .AddEntityFrameworkStores<AppDbContext>()
-                .AddDefaultTokenProviders();
-
+				.AddEntityFrameworkStores<AppDbContext>()
+				.AddDefaultTokenProviders();
 
 builder.Services.ConfigureApplicationCookie(opt => opt.LoginPath = "/Account/Login");
 builder.Services.ConfigureApplicationCookie(opt => opt.LogoutPath = "/Account/Logout");
 
+builder.Services.InitializeRepositories();
+builder.Services.InitializeServices();
 
-//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-//    .AddCookie(options =>
-//    {
-//        options.Cookie.Name = "ApplicationCookie";
-//        options.LoginPath = new PathString("/Account/Login");
-//        options.AccessDeniedPath = new PathString("/Account/Login");
-//        options.LogoutPath = new PathString("/Account/Logout");
-//    });
-
-
-builder.Services.AddScoped<IBaseRepository<Product>, ProductRepository>();
-builder.Services.AddScoped<IProductService, ProductService>();
-
-builder.Services.AddScoped<IBaseRepository<Cart>, CartRepostiroy>();
-builder.Services.AddScoped<ICartService, CartService>();
-
-builder.Services.AddScoped<IBaseRepository<Order>, OrderRepository>();
-builder.Services.AddScoped<IOrderService, OrderService>();
-
-builder.Services.AddScoped<IBaseRepository<User>, UserRepository>();
-builder.Services.AddScoped<IAccountService, AccountService>();
-
-builder.Services.AddScoped<IBaseRepository<UserAccount>, UserAccountRepository>();
-builder.Services.AddScoped<IUserAccountService, UserAccountService>();
-
-builder.Services.AddScoped<IBaseRepository<Contact>, ContactRepository>();
-builder.Services.AddScoped<IContactService, ContactService>();
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -99,7 +70,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Product}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Product}/{action=Index}/{id?}");
 
 app.Run();
