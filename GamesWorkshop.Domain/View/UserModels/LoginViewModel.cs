6d311c643dd0b6cafa2168d3b15e0c47da15fdf4
99Astral99 <strik4462@gmail.com>
2023-03-03 19:31:59 +0300

    LoginViewModel hotfix

--- GamesWorkshop.Domain/View/UserModels/LoginViewModel.cs
+++ GamesWorkshop.Domain/View/UserModels/LoginViewModel.cs
@@ -6,11 +6,10 @@ namespace GamesWorkshop.Domain.View.UserModels
     {
         [Display(Name = "Email address")]
         [Required(ErrorMessage = "The email address is required")]
-        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [EmailAddress(ErrorMessage = "Invalid Email or password")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
-        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
